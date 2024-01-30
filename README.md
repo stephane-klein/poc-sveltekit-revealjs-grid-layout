85025d5c083744d693d1dd99d10188be926eb67a
Stéphane Klein <contact@stephane-klein.info>
2024-01-30 14:38:34 +0100

    Reveal.js integration

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # POC to setup Reveal.js in SvelteKit projet + Grid Slide layout based on CSS grid
 
 Context issue in French: https://github.com/stephane-klein/backlog/issues/369
+
+Getting started:
+
+```sh
+$ rtx install
+$ pnpm install
+$ pnpn run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount, tick } from "svelte";
+    import "reveal.js/dist/reveal.css";
+    import "reveal.js/dist/theme/black.css";
+
+    onMount(async () => {
+        await tick();
+        const Reveal = (await import("reveal.js")).default;
+        console.log(Reveal);
+
+        const deck = new Reveal();
+        deck.initialize();
+    });
+</script>
+
+<svelte:head>
+    <title>Slide super titre</title>
+</svelte:head>
+
+<div class="reveal">
+    <div class="slides">
+        <section>
+            <h2>Titre 1</h2>
+        </section>
+        <section>
+            <h2>Titre 2</h2>
+        </section>
+    </div>
+</div>
