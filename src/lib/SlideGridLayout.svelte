8d8d862d178e6a96e14c8093e5a6763cc4cb6d5f
Stéphane Klein <contact@stephane-klein.info>
2024-01-30 15:40:01 +0100

    Improve grid layout

---
+++ src/lib/SlideGridLayout.svelte
@@ -0,0 +1,90 @@
+<div class="slide-grid-layout">
+    <div class="top-left">
+        <slot name="top-left" />
+    </div>
+    <div class="top">
+        <slot name="top" />
+    </div>
+    <div class="top-right">
+        <slot name="top-right" />
+    </div>
+    <div class="main">
+        <slot name="main" />
+    </div>
+    <div class="bottom-left">
+        <slot name="bottom-left" />
+    </div>
+    <div class="bottom">
+        <slot name="bottom" />
+    </div>
+    <div class="bottom-right">
+        <slot name="bottom-right" />
+    </div>
+</div>
+<style>
+    .slide-grid-layout {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        grid-template-rows: auto 1fr auto;
+        grid-template-areas: "topleft top topright"
+            "main main main"
+            "bottomleft bottom bottomright";
+    }
+
+    .top-left, .top, .top-right, .bottom-left, .bottom, .bottom-right {
+        font-size: 0.5em;
+    }
+    .top-left {
+        grid-area: topleft;
+        display: flex;
+        align-items: start;
+        justify-content: start;
+        height: 100%;
+    }
+
+    .top {
+        grid-area: top;
+        display: flex;
+        align-items: start;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .top-right {
+        grid-area: topright;
+        display: flex;
+        align-items: start;
+        justify-content: end;
+        height: 100%;
+    }
+
+    .main {
+        grid-area: main;
+    }
+
+    .bottom-left {
+        grid-area: bottomleft;
+        display: flex;
+        align-items: start;
+        justify-content: end;
+        height: 100%;
+    }
+
+    .bottom {
+        grid-area: bottom;
+        display: flex;
+        align-items: end;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .bottom-right {
+        grid-area: bottomright;
+        display: flex;
+        align-items: end;
+        justify-content: end;
+        height: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,16 @@
     import "reveal.js/dist/reveal.css";
     import "reveal.js/dist/theme/black.css";
 
+    import SlideGridLayout from "$lib/SlideGridLayout.svelte";
+
     onMount(async () => {
         await tick();
         const Reveal = (await import("reveal.js")).default;
-        console.log(Reveal);
 
-        const deck = new Reveal();
+        const deck = new Reveal({
+            slideNumber: true,
+            center: false
+        });
         deck.initialize();
     });
 </script>
@@ -20,10 +24,37 @@
 <div class="reveal">
     <div class="slides">
         <section>
-            <h2>Titre 1</h2>
+            <SlideGridLayout>
+                <span slot="main" class="center"><h2>Title 2</h2></span>
+                <span slot="bottom-left"><img src="https://sklein.xyz/_app/immutable/assets/avatar.iAdKzKwE.avif" /></span>
+            </SlideGridLayout>
         </section>
         <section>
-            <h2>Titre 2</h2>
+            <SlideGridLayout>
+                <span slot="top-left">Top left</span>
+                <span slot="top">Top middle</span>
+                <span slot="top-right">Top right</span>
+                <span slot="main" class="center"><h2>Title 2</h2></span>
+                <span slot="bottom-left"><img src="https://sklein.xyz/_app/immutable/assets/avatar.iAdKzKwE.avif" /></span>
+                <span slot="bottom">Bottom middle</span>
+                <span slot="bottom-right">Bottom right</span>
+            </SlideGridLayout>
         </section>
     </div>
 </div>
+
+<style>
+    :global(.reveal .slides > section) {
+        height: 100%;
+    }
+
+    .center {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+    :global(.top-left img, .top img, .top-right img, .bottom-left img, .bottom img, .bottom-right img) {
+        margin: 0;
+     }
+</style>
